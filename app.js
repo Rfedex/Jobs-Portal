fce1c06b85d3611f12fadfc9042157a4b3e9910a
Himanshu <himanshu.2231999@gmail.com>
2022-03-29 20:11:27 +0530

    Setup of User Model, Register Controller and connected to MongoDB

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 require('dotenv').config()
 require('express-async-errors')
 
+
 const express=require('express');
 const app=express()
+
+const connectDB=require('./db/connect')
+
+const authRouter=require('./routes/auth')
+const jobsRouter=require('./routes/jobs')
 
 //error handlers
 const errorHandlerMiddleware=require('./middleware/error-handler')
@@ -11,9 +17,8 @@ const notFoundMiddleware=require('./middleware/not-found')
 app.use(express.json());
 
 //routes
-app.get('/',(req,res)=>{
-    res.send("Jobs API");
-})
+app.use('/api/v1/auth/',authRouter)
+app.use('/api/v1/jobs/',jobsRouter)
 
 app.use(errorHandlerMiddleware)
 app.use(notFoundMiddleware)
@@ -22,6 +27,7 @@ const port=process.env.PORT || 3000
 
 const start= async ()=>{
     try {
+        await connectDB(process.env.MONGO_URI)
         app.listen(port,console.log(`Server is listening on port ${port}...`))
     } catch (error) {
         console.log(error);
