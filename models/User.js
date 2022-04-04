c692e9c26fefe2ead01720bcf861ae6e6ce17b32
Himanshu <himanshu.2231999@gmail.com>
2022-04-04 20:29:37 +0530

    Register and Login Controller setup

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose=require('mongoose')
 const bcrypt=require('bcryptjs')
+const jwt=require('jsonwebtoken')
 
 const UserSchema= new mongoose.Schema({
     name:{
@@ -22,9 +23,17 @@ const UserSchema= new mongoose.Schema({
     }
 })
 
+//Mongoose Middleware
 UserSchema.pre('save', async function(){
     const salt=await bcrypt.genSalt(10)
     this.password=await bcrypt.hash(this.password,salt)
 })
+
+//Schema Instance Method
+UserSchema.methods.createJWT= function(){
+    return jwt.sign({userId:this._id,name:this.name},process.env.JWT_SECRET,{
+        expiresIn:process.env.JWT_LIFETIME
+    })
+}
 
 module.exports=mongoose.model('User',UserSchema)
