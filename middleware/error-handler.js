5456133e89c4ee39002df96d634dcbf3287f8ece
Himanshu <himanshu.2231999@gmail.com>
2022-05-19 12:36:34 +0530

    Error Handling and Security patchup done

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,11 +1,32 @@
-const {CustomAPIError}=require('../errors')
 const {StatusCodes}=require('http-status-codes')
 
 const errorHandlerMiddleware=(err,req,res,next)=>{
-    if(err instanceof CustomAPIError){
-        return res.status(err.statusCode).json({msg:err.message})
+    let customError={
+        //setting defaults
+        statusCode:err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        msg:err.message || `Something went wrong, please try again later`
     }
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({err})
+    
+    //Validation Error
+    if(err.name==='ValidationError'){
+        customError.msg=Object.values(err.errors).map((item)=>item.message).join(',')
+        customError.statusCode=400
+    }
+
+    //Duplicate Error
+    if(err && err.code===11000){
+        customError.msg=`Duplicate value entered for ${Object.keys(err.keyValue)} feild, please use another value`
+        customError.statusCode=400
+    }
+
+    //Cast Error
+    if(err.name==='CastError'){
+        customError.msg=`No job with id ${err.value}`
+        customError.statusCode=404
+    }
+    //return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({err})
+    return res.status(customError.statusCode).json({msg:customError.msg})
+
 }
 
 module.exports=errorHandlerMiddleware
